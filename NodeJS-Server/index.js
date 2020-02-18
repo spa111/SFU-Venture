5847b483ceff8f97b0f7a8469012a79a3dbc6c91
Sukhdeep Parmar <spa111@sfu.ca>
2020-02-18 02:08:36 -0800

    add cors module to allow different browsers to access nodeJS API

--- NodeJS-Server/index.js
+++ NodeJS-Server/index.js
@@ -4,12 +4,17 @@ const database = require('./db_queries');
 const app = express();
 const port = 3000;
 
+// Use CORS to accept a request coming from any browser
+const isProduction = false;
+const origin = { origin: isProduction ? 'Deployed web server ip address here' : '*' };
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 );
+app.use(cors(origin));
 
 // Application endpoints
 app.get('/', (request, response) => {
