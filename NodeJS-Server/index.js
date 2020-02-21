a1c7a0877e7f542cbd502bb4bf4cb8a70e53e11e
Sukhdeep Parmar <spa111@sfu.ca>
2020-02-20 20:36:26 -0800

    Create email service, database sql, and rename api's
    This commit adds an api that will accept user signup details and email them with a way to verify their registration. It also adds more sql tables for the other features of the application. Finally, it changes the api names to look for /api

--- NodeJS-Server/index.js
+++ NodeJS-Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const database = require('./db_queries');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -22,11 +23,42 @@ app.get('/', (request, response) => {
     });
 });
 
-app.get('/users', cors(), database.getUsers);
-app.get('/users/:id', cors(), database.getUserById);
-app.post('/users', cors(), database.createUser);
-app.put('/users/:id', cors(), database.updateUser);
-app.delete('/users/:id', cors(), database.deleteUser);
+app.get('/api/users', cors(), database.getUsers);
+app.get('/api/users/:id', cors(), database.getUserById);
+app.post('/api/users', cors(), database.createUser);
+app.put('/api/users/:id', cors(), database.updateUser);
+app.delete('/api/users/:id', cors(), database.deleteUser);
+
+app.post('/api/signup', cors(), (request, response) => {
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'sfuventure470@gmail.com',
+            pass: 'ventureABC123'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    
+    var mailOptions = {
+        from: 'sfuventure470@gmail.com',
+        to: 'sparm30@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+            response.status(535).json(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            response.json('Email sent: ' + info.response);
+        }
+    });
+});
 
 // Start NodeJS server and list on port 3000 for requests
 app.listen(port, () => {
---
+++ Postgres-Database-Details/cmpt470 schema.sql
@@ -0,0 +1,52 @@
+CREATE  TABLE users ( 
+	id                   bigserial  NOT NULL ,
+	fullname             text  NOT NULL ,
+	password             text  NOT NULL ,
+	email                text  NOT NULL ,
+	username             text   ,
+	is_email_verified    bool DEFAULT false  ,
+	is_admin             bool DEFAULT false  ,
+	is_student           bool DEFAULT false  ,
+	is_faculty           bool DEFAULT false  ,
+	CONSTRAINT pk_users_id PRIMARY KEY ( id ),
+	CONSTRAINT unique_email UNIQUE ( email ) ,
+	CONSTRAINT unique_username UNIQUE ( username ) 
+ );
+
+CREATE  TABLE marketplace ( 
+	id                   bigserial  NOT NULL ,
+	listing_user_id      bigint  NOT NULL ,
+	listing_title        text  NOT NULL ,
+	listing_description  text   ,
+	listing_price        money DEFAULT 0 NOT NULL ,
+	listing_contact_info text   ,
+	CONSTRAINT pk_marketplace_listing_id PRIMARY KEY ( id )
+ );
+
+ALTER TABLE marketplace ADD CONSTRAINT fk_marketplace_listing_users FOREIGN KEY ( listing_user_id ) REFERENCES users( id ) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE  TABLE activities ( 
+	id                   bigserial  NOT NULL ,
+	poster_user_id       bigint  NOT NULL ,
+	corresponding_department text  NOT NULL ,
+	activity_title       text  NOT NULL ,
+	activity_description text  NOT NULL ,
+	"activity-price"     money DEFAULT 0 NOT NULL ,
+	activity_location    text  NOT NULL ,
+	"activity-timestamp" timestamp DEFAULT current_timestamp NOT NULL ,
+	CONSTRAINT pk_activities_id PRIMARY KEY ( id )
+ );
+
+ALTER TABLE activities ADD CONSTRAINT fk_poster_user_id FOREIGN KEY ( poster_user_id ) REFERENCES users( id ) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE  TABLE volunteer_board ( 
+	id                   bigserial  NOT NULL ,
+	user_id              bigint  NOT NULL ,
+	volunteer_title      text  NOT NULL ,
+	volunteer_description text  NOT NULL ,
+	volunteer_location   text  NOT NULL ,
+	volunteer_timestamp  timestamp DEFAULT current_timestamp NOT NULL ,
+	CONSTRAINT pk_volunteer_board_id PRIMARY KEY ( id )
+ );
+
+ALTER TABLE volunteer_board ADD CONSTRAINT fk_user_id FOREIGN KEY ( user_id ) REFERENCES users( id ) ON DELETE CASCADE ON UPDATE CASCADE;
