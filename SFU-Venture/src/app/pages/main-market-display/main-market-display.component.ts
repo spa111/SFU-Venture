a2097d17db599d1523308deeeb25a530dd78019f
Brian Marwood <bmarwood@sfu.ca>
2020-03-08 22:16:51 -0700

    Add in Basic Dynamic post item handling for marketplace, Add in photo URL handler

--- SFU-Venture/src/app/pages/main-market-display/main-market-display.component.ts
+++ SFU-Venture/src/app/pages/main-market-display/main-market-display.component.ts
@@ -10,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 
 export class MainMarketDisplayComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit() {
   } 
+
+  textbooks : any;
+
+  constructor(){
+
+      this.textbooks = [{
+          textbookName : "Operating System Concepts, 9th Edition, Silberschatz, Galvin, Gagne, 2012",
+          course : "300",
+          faculty : "CMPT",
+          price: "10",
+          postDate : "August 23",
+          imageUrl : "https://picsum.photos/200"
+      },
+      {
+        textbookName : "Operatding System Concepsts, 9th Edition, Silberschatz, Galvin, Gagne, 2012",
+          course : "300",
+          faculty : "CMPT",
+          price: "10",
+          postDate : "August 23",
+          imageUrl : "https://picsum.photos/200"
+
+      },
+      {
+        textbookName : "Opedrating System Concepts, 9th Edition, Silberschatz, Galvin, Gagne, 2012",
+        course : "300",
+        faculty : "CMPT",
+        price: "10",
+        postDate : "August 23",
+        imageUrl : "https://picsum.photos/200"
+
+      },
+      {
+        textbookName : "Operating System Conceptss, 9th Edition, Silberschatz, Galvin, Gagne, 2012",
+        course : "300",
+        faculty : "CMPT",
+        price: "10",
+        postDate : "August 23",
+        imageUrl : "https://picsum.photos/200"
+      }]
+  };
 
 }
