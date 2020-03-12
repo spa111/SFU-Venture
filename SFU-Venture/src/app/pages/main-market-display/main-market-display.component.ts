0baac3104646cf0d4258501d022bf492963a56b8
Brian Marwood <bmarwood@sfu.ca>
2020-03-11 23:17:28 -0700

    Add Post button, Fix search button searching over an already searched array

--- SFU-Venture/src/app/pages/main-market-display/main-market-display.component.ts
+++ SFU-Venture/src/app/pages/main-market-display/main-market-display.component.ts
@@ -1,82 +1,78 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
-import { Router } from '@angular/router'; 
-import { TextbooksService } from '../../services/server-apis/textbooks/textbooks.service'
-declare var $:any;
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { AuthService } from "../../services/auth/auth.service";
+import { Router } from "@angular/router";
+import { TextbooksService } from "../../services/server-apis/textbooks/textbooks.service";
+declare var $: any;
 
 @Component({
-  selector: 'app-main-market-display',
-  templateUrl: './main-market-display.component.html',
-  styleUrls: ['./main-market-display.component.scss']
+  selector: "app-main-market-display",
+  templateUrl: "./main-market-display.component.html",
+  styleUrls: ["./main-market-display.component.scss"]
 })
-
 export class MainMarketDisplayComponent implements OnInit {
-  
-
   constructor(
-    private router: Router, 
+    private router: Router,
     private authService: AuthService,
-    private textbooksService: TextbooksService,
-  ) {     
-    // this.getAllTextbooks();
-  }
+    private textbooksService: TextbooksService
+  ) {}
 
-  getAllTextbooks(){
+  getAllTextbooks() {
     console.log("full Texbook Retrieval called");
-    this.textbooksService.getAll().then((result) => {
-      console.log("Retrieval Successful");
-      console.log(result);
-      this.textbooks = result;
-
-
-    }).catch(err => {
-      console.log(err);
-      // Display the login-alert box
-    });
+    this.textbooksService
+      .getAll()
+      .then(result => {
+        console.log("Retrieval Successful");
+        console.log(result);
+        this.textbooks = result;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-  
+
   ngOnInit() {
     this.getAllTextbooks();
-    this.textbooksService.getDept().then((result) => {
-      console.log(result);
-      this.faculties = result;
-      
-      this.faculties.forEach(faculty => {
-        faculty.textbooks = this.textbooks.filter(textbook => {
-          return textbook.faculty_name.toLocaleLowerCase() == faculty.value;
+    this.textbooksService
+      .getDept()
+      .then(result => {
+        console.log(result);
+        this.faculties = result;
+
+        this.faculties.forEach(faculty => {
+          faculty.textbooks = this.textbooks.filter(textbook => {
+            return textbook.faculty_name.toLocaleLowerCase() == faculty.value;
+          });
         });
+
+        this.facultiesDOM = JSON.parse(JSON.stringify(this.faculties));
+      })
+      .catch(err => {
+        console.log(err);
       });
-      
-      this.facultiesDOM = JSON.parse(JSON.stringify(this.faculties));
-    })
-    .catch((err) => {
-      console.log(err);
-    })
   }
-  
+
   textbooks: any;
   faculties: any;
   facultiesDOM: any;
-  // dept: Array<Array<any>> = [];
-  
-  sortByDept(){
-    console.log($('#filterDept')[0].value);
-    let filter_value = $('#filterDept')[0].value;
+
+  sortByDept() {
+    console.log($("#filterDept")[0].value);
+    let filter_value = $("#filterDept")[0].value;
 
     if (filter_value == "choose") {
-      this.facultiesDOM = JSON.parse(JSON.stringify(this.faculties));
+      this.reset();
     } else {
+      this.facultiesDOM = JSON.parse(JSON.stringify(this.faculties));
       this.facultiesDOM = this.facultiesDOM.filter(filter => {
-        return filter.value == filter_value; 
+        return filter.value == filter_value;
       });
     }
   }
 
-  reset(){
+  reset() {
     this.facultiesDOM = JSON.parse(JSON.stringify(this.faculties));
-    $('#filterDept')[0].value = $('#filterDept')[0][0].value;
-    console.log($('#filterDept')[0].value);
-
+    $("#filterDept")[0].value = $("#filterDept")[0][0].value;
+    console.log($("#filterDept")[0].value);
   }
   //   {
   //     textbookName: "C++ Primer (5th Edition)",
@@ -95,6 +91,4 @@ export class MainMarketDisplayComponent implements OnInit {
   //     postDate: "Feburary 14",
   //     imageUrl: "https://images-na.ssl-images-amazon.com/images/I/61pHgCDCgqL.jpg"
   //   }]
-
-
 }
