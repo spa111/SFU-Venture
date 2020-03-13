4217957dbd05be88718720788209f5c069e825db
spa111 <spa111@sfu.ca>
2020-03-13 11:22:05 -0700

    Fix background video not playing all the time
    fixes video not loading all the time at the start and also makes the buttons change depending on whether you are logged in or not. Logged out shows a login button and loggedIn shows the market button

--- SFU-Venture/src/app/pages/home-page/home-page.component.ts
+++ SFU-Venture/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/server-apis/users/users.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,9 +9,11 @@ import { UsersService } from '../../services/server-apis/users/users.service';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private userService : UsersService) { }
+  loggedIn: Boolean = false;
+
+  constructor(private authService : AuthService) { }
 
   ngOnInit() {
-    // Console.log the users on the homepage for connection testing
+    this.loggedIn = this.authService.loggedIn;
   }
 }
