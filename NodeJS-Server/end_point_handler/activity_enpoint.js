3db15adeef6a19daa2d274fcf873ff9f1e6b34dd
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2020-03-25 18:36:51 -0700

    fixed bug route call

--- NodeJS-Server/end_point_handler/activity_enpoint.js
+++ NodeJS-Server/end_point_handler/activity_enpoint.js
@@ -5,6 +5,6 @@ const activityFinder = require('../db_handler/actvitiy_finder_controller');
 
 const router = express.Router()
 
-router.post('/api/activity/', cors(), activityFinder.getActivityAroundSFU);
+router.post('/api/activity/', cors(), activityFinder.getActivityAround);
 
 module.exports = router
