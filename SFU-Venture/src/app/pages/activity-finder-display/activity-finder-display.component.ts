b239ecf132d6de47bfc227022acc286603ab1160
fchandel <fchandel@sfu.ca>
2020-03-27 18:45:03 -0700

    updates UI of activity page

--- SFU-Venture/src/app/pages/activity-finder-display/activity-finder-display.component.ts
+++ SFU-Venture/src/app/pages/activity-finder-display/activity-finder-display.component.ts
@@ -1,5 +1,6 @@
 import { ActivitiesService } from './../../services/server-apis/activities/activities.service';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-activity-finder-display',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityFinderDisplayComponent implements OnInit {
   contentLoaded: Boolean = false;
-  
+  result: Array<any>
   constructor( private activitiesService: ActivitiesService) { 
   }
 
@@ -18,6 +19,26 @@ export class ActivityFinderDisplayComponent implements OnInit {
     var activities = await this.activitiesService.getAll();
     this.activitesDOM = activities
     this.contentLoaded = true;
+
+    this.activitesDOM.forEach(element => {
+      let date = moment(element.activity_timestamp)
+      let formattedDate = date.format("MMM Do YYYY")
+      console.log(formattedDate)
+      element.activity_timestamp = formattedDate
+    });
+
+    var groups = new Set(this.activitesDOM.map(item => item.activity_timestamp))
+    this.result = [];
+    groups.forEach(g =>
+      this.result.push({
+        name: g, 
+        values: this.activitesDOM.filter(i => i.activity_timestamp === g)
+      }
+    ))
+
+    console.log(this.result)
+    groups.forEach(g => console.log(g))
+
 
     console.log(activities)
   }
